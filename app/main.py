3c818a1304f1a9052504ba750113f0b3242e8adb
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2020-10-25 17:59:52 +0100

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from firebase_admin import credentials, firestore, initialize_app, auth
 from functools import wraps
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from app.services.user_service import UserService
 from app.services.team_service import TeamService
 from app.services.authentication_service import AuthenticationService
@@ -9,7 +10,7 @@ import json
 import sys
 
 app = Flask(__name__) 
-
+CORS(app)
 # Firebase variables initializers
 cred = credentials.Certificate("conf/key.json")
 firebase = initialize_app(cred)
